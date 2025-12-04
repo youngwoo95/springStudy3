f01bb487a146ee1113437c3af1287165c32b44a0
kyw <rladyddn258@gmail.com>
2025-12-04 18:58:42 +0900

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,22 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+
+
+}
+
