7b24e6f3de9300b186a2bcfc93ef8ccf9bf343d6
kyw <rladyddn258@gmail.com>
2025-12-04 19:46:24 +0900

    회원 웹 기능 - 등록

--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,10 +1,13 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -16,7 +19,24 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
 
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 
 }
 
